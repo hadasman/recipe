13eed2c4b025aea8dd2a267ad2790077466ae0ae
hadasman <hadas.manor@mail.huji.ac.il>
2021-03-06 14:50:16 +0200

    Added option to select file, step buttons by line in file, and buttons colored by click order

--- recipe.js
+++ recipe.js
@@ -6,11 +6,9 @@ function template(){
 	// document.getElementById("minutes").innerHTML = minutes;
 	// document.getElementById("seconds").innerHTML = seconds;
 }
-var item_number = 0;
-var tot_num_items = 4;
+var line_number = 0;
 function next_item(){
-	
-	if (item_number>=tot_num_items){
+	if (line_number>=num_lines){
 		Notification.requestPermission().then(permission => {
 			if (permission==="granted"){
 				console.log(permission);
@@ -18,17 +16,125 @@ function next_item(){
 				}
 		})}
 	else{
-		item_number = item_number + 1;
-		console.log(`button${item_number}`);
+		line_number = line_number + 1;
+		console.log(`button${line_number}`);
 
 		// Buttons
-		document.querySelector(`#button${item_number}`).style.color = "#1C2340";
+		// document.querySelector(`#button${line_number}`).style.color = "#1C2340";
+		document.querySelector(`#step_button${line_number}`).style.color = "#1C2340";
 		}
 	}
 
 
+var input = document.querySelector('input')
+
+
+input.addEventListener('change', function(e){
+	var reader = new FileReader();
+
+	reader.onload = function() {
+		lines = reader.result.split('\n').map(function(line){
+			return line.split(',');
+		})
+
+		// var docFrag = document.createDocumentFragment();
+		const newDiv = document.createElement("div");
+		for (var i=0; i < lines.length ; i++){
+		     var elem = document.createElement('input');
+		     elem.type = 'button';
+		     elem.value = lines[i];
+		     elem.onclick = next_item;
+		     elem.className = "fas fa-wave-square fa-2x justify-content-center align-items-center";
+		     elem.style.position = "relative";
+		     elem.style.right = "2px";
+		     elem.style.float = "center";
+		     elem.id = `step_button${i+1}`;
+		     elem.style.background = "#1C2340";
+		     // docFrag.appendChild(elem);
+		     newDiv.appendChild(elem);
+		}
+		// document.body.appendChild(docFrag);
+		document.body.appendChild(newDiv);
+		num_lines = lines.length;
+	}
+	reader.readAsText(input.files[0]);	
+
+}, false)
+
+
+// document.getElementById("file").addEventListener('change', function(){
+// 	var fr = new FileReader();
+// 	fr.onload = function(){
+// 		document.getElementById("FileContents").textContent = this.result;
+// 	}
+// 	fr.readAsText(this.files[0]);
+// })
+
+
+// let input = document.querySelector('input');
+// let textarea = document.querySelector('textarea');
+ 
+// input.addEventListener('change', () => {
+//     let files = input.files;
+ 
+//     if(files.length == 0) return;
+ 
+//     const file = files[0];
+ 
+//     let reader = new FileReader();
+ 
+//     reader.onload = (e) => {
+//         const file = e.target.result;
+//         const lines = file.split(/\r\n|\n/);
+//         textarea.value = lines.join('\n');
+//     };
+ 
+//     reader.onerror = (e) => alert(e.target.error.name);
+ 
+//     reader.readAsText(file); 
+    
+// });
+
+
+// console.log(document.querySelector('input'));
+// var input = document.querySelector('input');
+// input.addEventListener('change', () => {
+// 	let files = input.files;
+	
+// 	if (files.length==0) return;
+// 	const file = files[0];
+// 	let reader = new FileReader();
+// 	reader.onload = (e) => {
+// 		const file = e.file.result;
+// 		const lines = lines.split(/\r\n|\n/);
+// 		console.log(lines.join('\n'));
+// 	};
+// 	reader.readAsText(file);
+
+// });
+
+
+// if (document.getElementById('file')){
+// document.getElementById('file').onchange = function(){
+
+//   var file = this.files[0];
+
+//   var reader = new FileReader();
+//   reader.onload = function(progressEvent){
+//     // Entire file
+//     console.log(this.result);
+
+//     // By lines
+//     var lines = this.result.split('\n');
+//     for(var line = 0; line < lines.length; line++){
+//       console.log(lines[line]);
+//     }
+//   };
+//   reader.readAsText(file);
+// 	};
 
 
 
+// }
 
 
