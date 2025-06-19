73325e42b68f2bf16dcb9370f678a341f028f656
hadasmanor <hadasmanor91@gmail.com>
2025-06-20 01:37:42 +0300

    changed format

--- recipe.js
+++ recipe.js
@@ -2,159 +2,90 @@ Notification.requestPermission();
 if (Notification.permission!=="granted"){
 	Notification.requestPermission();
 }
-function template(){
-	// document.getElementById("minutes").innerHTML = minutes;
-	// document.getElementById("seconds").innerHTML = seconds;
-}
+
 var line_number = 0;
 function next_item(n){
-	if (line_number==num_lines-1){
+	line_number = `${n}`;
+	if (line_number==num_lines){
 		Notification.requestPermission().then(permission => {
 			if (permission==="granted"){
 				console.log(permission);
 				const notification = new Notification("Recipe Completed!", {body: "Recipe Completed!", title:"..."})
 				}
 		})}
-	else{
-			line_number = `${n}`;
+	else{			
 			console.log(line_number);
 			console.log(`button${line_number}`);
+            const targetButton = document.querySelector(`#step_button${line_number}`);
+            const targetCheckMark = document.querySelector(`#check_div${line_number}`);
+			if (targetButton.style.OnOff!="Off"){
+					targetButton.style.color = "#F0DAC5";
+					targetButton.style.height = "0";
+					targetButton.style.OnOff = "Off";
+					targetCheckMark.style.display = 'inline-block'
 
-			if (document.querySelector(`#step_button${line_number}`).style.OnOff!="Off"){		// line_number = line_number + 1;
-			
-					// Buttons
-					document.querySelector(`#step_button${line_number}`).style.color = "#1C2340";
-					document.querySelector(`#step_button${line_number}`).style.OnOff = "Off";
 					console.log(document.querySelector(`#step_button${line_number}`).style.OnOff);
 			}else{
-					document.querySelector(`#step_button${line_number}`).style.color = "#AEF359";
-					document.querySelector(`#step_button${line_number}`).style.OnOff = "On";
+					targetButton.style.color = "#c576ac";
+					targetButton.style.height = "100px";
+					targetButton.style.OnOff = "On";
+                    targetCheckMark.style.display = "none";
 
 			}	
 		}
 	}
 
-
 var input = document.querySelector('input')
-
-
 input.addEventListener('change', function(e){
-	var reader = new FileReader();
+var reader = new FileReader();
 
-	reader.onload = function() {
-		lines = reader.result.split('\n').map(function(line){
-			return line.split(',');
-		})
+reader.onload = function() {
+    const lines = reader.result.split('\n').map(function(line){
+        return line.split(',');
+    })
 
-		// var docFrag = document.createDocumentFragment();
-		const newDiv = document.createElement("div");
-		for (var i=0; i < lines.length ; i++){
-		     var elem = document.createElement('input');
-		     elem.type = 'button';
-		     elem.value = lines[i];
-		     const st = `${i+1}`;
-		     elem.onclick = function () {
-    next_item(st);
-};;
-		     elem.className = "wrapped_div fas fa-wave-square fa-2x justify-content-center align-items-center";
-		     elem.style.position = "relative";
-		     elem.style.right = "2px";
-		     elem.style.float = "center";
-		     elem.id = `step_button${i+1}`;		     		     
-		     elem.style.background = "#1C2340";
-		     // elem.style.width = "400px";
-		     elem.style.height = "100px";
-		     // elem.display = "inline-block";
-		     // elem.style.overflowWrap = "anywhere";
-		     // elem.style.wordWrap = "break-word";
-		     // elem.style.lineBreak = "strict";
+    const ingredients_div = document.createElement("ul");
+    ingredients_div.dir = "rtl"
+    ingredients_div.className = "ingredient_list"
+    const ingredients = lines[0]
+    ingredients.forEach(ingredient =>{
+        const ingredients_elem = document.createElement("li")
+        ingredients_elem.textContent = ingredient
+        ingredients_elem.className = "ingredient_list_item"
+        ingredients_div.appendChild(ingredients_elem)
+    })
 
+    const buttonsDiv = document.createElement("div");
+    for (var i=1; i < lines.length ; i++){
+        var button = document.createElement('input');
+        button.type = 'button';
+        button.value = lines[i];
+        const st = `${i+1}`;
+        button.onclick = function () {
+            next_item(st);
+        };
+        button.className = "step_button";
+        button.style.position = "relative";
+        button.style.right = "2px";
+        button.id = `step_button${i+1}`;
 
-		     // docFrag.appendChild(elem);
-		     newDiv.appendChild(elem);
-		}
-		// document.body.appendChild(docFrag);
-		document.body.appendChild(newDiv);
-		num_lines = lines.length;
-	}
-	reader.readAsText(input.files[0]);	
+        var checkDiv = document.createElement("div")
+        checkDiv.innerHTML = '&#10003'
+        checkDiv.style.color = 'green';
+        checkDiv.style.fontSize = '24px';
+        checkDiv.style.display = "none";
+        checkDiv.id = `check_div${i+1}`
+        const buttonDiv = document.createElement("div");
+        buttonDiv.appendChild(button);
+        buttonDiv.appendChild(checkDiv);
+        buttonDiv.style.display = "flex";
+        buttonsDiv.appendChild(buttonDiv);
+    }
+
+document.body.appendChild(ingredients_div);
+document.body.appendChild(buttonsDiv);
+num_lines = lines.length;
+}
+reader.readAsText(input.files[0]);
 
 }, false)
-
-
-// document.getElementById("file").addEventListener('change', function(){
-// 	var fr = new FileReader();
-// 	fr.onload = function(){
-// 		document.getElementById("FileContents").textContent = this.result;
-// 	}
-// 	fr.readAsText(this.files[0]);
-// })
-
-
-// let input = document.querySelector('input');
-// let textarea = document.querySelector('textarea');
- 
-// input.addEventListener('change', () => {
-//     let files = input.files;
- 
-//     if(files.length == 0) return;
- 
-//     const file = files[0];
- 
-//     let reader = new FileReader();
- 
-//     reader.onload = (e) => {
-//         const file = e.target.result;
-//         const lines = file.split(/\r\n|\n/);
-//         textarea.value = lines.join('\n');
-//     };
- 
-//     reader.onerror = (e) => alert(e.target.error.name);
- 
-//     reader.readAsText(file); 
-    
-// });
-
-
-// console.log(document.querySelector('input'));
-// var input = document.querySelector('input');
-// input.addEventListener('change', () => {
-// 	let files = input.files;
-	
-// 	if (files.length==0) return;
-// 	const file = files[0];
-// 	let reader = new FileReader();
-// 	reader.onload = (e) => {
-// 		const file = e.file.result;
-// 		const lines = lines.split(/\r\n|\n/);
-// 		console.log(lines.join('\n'));
-// 	};
-// 	reader.readAsText(file);
-
-// });
-
-
-// if (document.getElementById('file')){
-// document.getElementById('file').onchange = function(){
-
-//   var file = this.files[0];
-
-//   var reader = new FileReader();
-//   reader.onload = function(progressEvent){
-//     // Entire file
-//     console.log(this.result);
-
-//     // By lines
-//     var lines = this.result.split('\n');
-//     for(var line = 0; line < lines.length; line++){
-//       console.log(lines[line]);
-//     }
-//   };
-//   reader.readAsText(file);
-// 	};
-
-
-
-// }
-
-
